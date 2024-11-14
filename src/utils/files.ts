113ab29b52231ba39e1a90d0587ab8c3e1c8eb44
lucaserib <lucasemanuelribeiro@hotmail.com>
2024-11-14 11:22:33 -0300

    function of counting implemented

--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,6 +1,7 @@
 import fs from "fs/promises";
 import Link from "../models/Link";
 import { read } from "fs";
+import { stringify } from "querystring";
 
 const dataBasePath = "src/database.json";
 
@@ -14,6 +15,28 @@ export async function readData(): Promise<Link[]> {
 export async function addData(link: Link) {
   const datas = await readData();
   datas.push(link);
+
+  await fs.writeFile(dataBasePath, JSON.stringify(datas, null, "\t"));
+}
+
+export async function countVisits(identifier: string) {
+  const datas = await readData();
+
+  const link = datas.find((link) => {
+    return link.identifier === identifier;
+  });
+
+  const linkVisits: Link = {
+    identifier: link!.identifier,
+    url: link!.url,
+    visits: link!.visits + 1,
+  };
+
+  const index = datas.findIndex((index) => {
+    return index.identifier === identifier;
+  });
+
+  datas.splice(index, 1, linkVisits);
 
   await fs.writeFile(dataBasePath, JSON.stringify(datas, null, "\t"));
 }
